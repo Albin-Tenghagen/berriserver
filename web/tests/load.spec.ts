1facf354016cfe4eb806deba50e742bbe34426b8
TheUnseenBug <dennis.granheimer@live.se>
2025-11-17 10:46:33 +0100

    added new load test

---
+++ web/tests/load.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("Measuring page load times", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  const perf = await page.evaluate(() => JSON.stringify(performance.timing));
+  const timings = JSON.parse(perf);
+
+  const loadTime = timings.loadEventEnd - timings.navigationStart;
+
+  expect(loadTime).toBeLessThan(3000);
+});
