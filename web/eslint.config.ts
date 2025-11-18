d103c067031780103d0695bec42ebcfa168bd512
TheUnseenBug <dennis.granheimer@live.se>
2025-11-18 11:37:33 +0100

    fix for linting

--- web/eslint.config.ts
+++ web/eslint.config.ts
@@ -1,11 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import js from "@eslint/js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { react },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  }
+);
