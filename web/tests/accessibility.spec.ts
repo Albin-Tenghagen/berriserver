6d4011ae816167c1340219d612544d908670c49e
TheUnseenBug <dennis.granheimer@live.se>
2025-11-19 14:34:53 +0100

    add accessability test

---
+++ web/tests/accessibility.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright"; // 1
+
+test.describe("homepage", () => {
+  // 2
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:5173/"); // 3
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
+
+    expect(accessibilityScanResults.violations).toEqual([]); // 5
+  });
+});
+
+test("should not have any automatically detectable WCAG A or AA violations", async ({
+  page,
+}) => {
+  await page.goto("localhost:5173/");
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
